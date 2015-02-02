864ba320e6d8b07e0370551e29531cd9179d823f
Aye Kwan <aye.kwan@gmail.com>
2015-02-02 16:36:52 +0700

    finished

--- app/src/main/java/th/ac/tu/siit/its333/lab3exercise1/CourseActivity.java
+++ app/src/main/java/th/ac/tu/siit/its333/lab3exercise1/CourseActivity.java
@@ -1,9 +1,14 @@
 package th.ac.tu.siit.its333.lab3exercise1;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
 
 public class CourseActivity extends ActionBarActivity {
@@ -14,6 +19,24 @@ public class CourseActivity extends ActionBarActivity {
         setContentView(R.layout.activity_course);
     }
 
+    public void addClicked(View v) {
+        //Return values to MainActivity
+        Intent code = new Intent();
+        EditText etCode = (EditText)findViewById(R.id.etCode);
+        code.putExtra("val1", etCode.getText().toString());
+
+        EditText etCR = (EditText)findViewById(R.id.etCR);
+        code.putExtra("val2", etCR.getText().toString());
+
+        RadioGroup rg = (RadioGroup)findViewById(R.id.rgroup);
+        RadioButton rb = (RadioButton)findViewById(rg.getCheckedRadioButtonId());
+
+        code.putExtra("val3", rb.getText().toString());
+        //Set the inten as the result of this activity
+        setResult(RESULT_OK, code);
+        //End this activity
+        finish();
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
--- app/src/main/java/th/ac/tu/siit/its333/lab3exercise1/MainActivity.java
+++ app/src/main/java/th/ac/tu/siit/its333/lab3exercise1/MainActivity.java
@@ -6,6 +6,8 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -21,6 +23,10 @@ public class MainActivity extends ActionBarActivity {
     List<Integer> listCredits;
     List<String> listGrades;
 
+    TextView TVGP;
+    TextView TVC;
+    TextView TVGPA;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,16 +36,113 @@ public class MainActivity extends ActionBarActivity {
         listCredits = new ArrayList<Integer>();
         listGrades = new ArrayList<String>();
 
+        TVGP = (TextView)findViewById(R.id.tvGP);
+        TVC = (TextView)findViewById(R.id.tvCR);
+        TVGPA = (TextView)findViewById(R.id.tvGPA);
         //Use listCodes.add("ITS333"); to add a new course code
         //Use listCodes.size() to refer to the number of courses in the list
     }
 
-    public void buttonClicked(View v) {
+    public double calGrade(){
+        double sum = 0.0;
+        for(int i = 0; i < listGrades.size(); i++){
+            sum += lookup(listGrades.get(i)) * listCredits.get(i);
+        }
+        return sum;
     }
+
+    public double lookup(String grade){
+
+        switch (grade){
+            case "A":
+                return 4.0;
+
+            case "B+":
+                return 3.5;
+            case "B":
+                return 3.0;
+            case "C+":
+                return 2.5;
+            case "C":
+                return 2.0;
+            case "D+":
+                return 1.5;
+            case "D":
+                return 1.0;
+            case "F":
+                return 0.0;
+
+
+
+        }
+        return 0;
+    }
+
+    public double calCredits(){
+        double sum = 0.0;
+        for(int i = 0; i < listCredits.size(); i++){
+            sum += listCredits.get(i);
+        }
+        return sum;
+    }
+
+    public void buttonClicked(View v) {
+        int id = v.getId();
+
+        switch(id) {
+            case R.id.button2:
+                Intent i = new Intent(this, CourseActivity.class);
+                startActivityForResult(i, 88);
+                break;
+            case R.id.button4:
+                Intent j = new Intent(this, CourseListActivity.class);
+                String s = "List of Courses\n";
+
+                for(int k = 0; k < listCodes.size(); k++) {
+                    s += listCodes.get(k) + "(" + listCredits.get(k) + "credits) = " + listGrades.get(k) +"\n";
+                    j.putExtra("xxx", s);
+                }
+                //putExtra attaches a value named "toAct3" to the intent.
+
+                startActivity(j);
+                break;
+            case R.id.button:
+                listCodes.clear();
+                listCredits.clear();
+                listGrades.clear();
+                TVGP.setText(calGrade() + "");
+                TVC.setText(calCredits() + "");
+                TVGPA.setText("0");
+                break;
+        }
+    }
+
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         // Values from child activity
+        if (requestCode == 88) {
+            if (resultCode == RESULT_OK) {
+                //Retrieve the returned value from Activity3
+                //use getStringExtra when the returned value is a string.
+                //Otherwise, we can use getIntExtra, getDoubleExtra
+                String val1 = data.getStringExtra("val1");
+                listCodes.add(val1);
+
+                Integer val2 = Integer.parseInt(data.getStringExtra("val2"));
+                listCredits.add(val2);
+
+                String val3 = data.getStringExtra("val3");
+                listGrades.add(val3);
+
+            }
+
+            TVGP.setText(calGrade() + "");
+            TVC.setText(calCredits() + "");
+            TVGPA.setText(calGrade()/calCredits() + "");
+
+        }
+
     }
 
     @Override
